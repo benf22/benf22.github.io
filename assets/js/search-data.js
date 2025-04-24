650dfefdb5ec35252cae5ecda6d181c388249012
Ben Fishman <benf22@gmail.com>
2025-04-24 09:36:19 +0300

    Add talks section

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-talks",
+          title: "talks",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/talks/";
+          },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
       
         title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
@@ -41,10 +48,56 @@ ninja.data = [{
     },{id: "news-i-started-my-ph-d-studies-sparkles-more-to-come-soon-smile",
           title: 'I started my Ph.D studies! :sparkles: More to come, soon… :smile:',
           description: "",
+          section: "News",},{id: "news-my-webinar-evaluation-of-ai-models-and-algorithms-was-accepted-to-the-imvc-pre-conferance-webinar-series-see-you-on-march-11-2025",
+          title: 'My webinar “Evaluation of AI models and Algorithms” was accepted to the IMVC...',
+          description: "",
           section: "News",},{id: "news-my-talk-bringing-ai-to-production-was-accepted-to-imvc-see-you-there-april-1-2025",
           title: 'My talk “Bringing AI to Production” was accepted to IMVC. See you there...',
           description: "",
-          section: "News",},{
+          section: "News",},{id: "news-my-lecture-from-imvc-2025-bringing-ai-to-production-is-now-live-you-can-now-watch-the-full-video-and-check-out-the-slide-deck-from-the-talk-click-here",
+          title: 'My lecture from IMVC 2025, “Bringing AI to Production,” is now live. You...',
+          description: "",
+          section: "News",},{id: "talks-prosodic-feature-criterion",
+          title: 'Prosodic Feature Criterion',
+          description: "",
+          section: "Talks",handler: () => {
+              window.location.href = "/talks/pfc-amazonday/";
+            },},{id: "talks-prosodic-feature-criterion",
+          title: 'Prosodic Feature Criterion',
+          description: "",
+          section: "Talks",handler: () => {
+              window.location.href = "/talks/pfc-weizman/";
+            },},{id: "talks-prosodic-feature-criterion",
+          title: 'Prosodic Feature Criterion',
+          description: "",
+          section: "Talks",handler: () => {
+              window.location.href = "/talks/pfc-ee-seminar/";
+            },},{id: "talks-panel-about-ai",
+          title: 'Panel about AI',
+          description: "",
+          section: "Talks",handler: () => {
+              window.location.href = "/talks/pfc-experts-panel/";
+            },},{id: "talks-extracting-multi-modal-insights-from-media-content",
+          title: 'Extracting multi-modal insights from media content',
+          description: "",
+          section: "Talks",handler: () => {
+              window.location.href = "/talks/vi-talk/";
+            },},{id: "talks-a-bit-of-ai",
+          title: 'A Bit of AI',
+          description: "",
+          section: "Talks",handler: () => {
+              window.location.href = "/talks/a-bit-of-ai/";
+            },},{id: "talks-evaluation-of-ai-models",
+          title: 'Evaluation of AI Models',
+          description: "",
+          section: "Talks",handler: () => {
+              window.location.href = "/talks/imvc-25-webinar/";
+            },},{id: "talks-bringing-ai-to-production-intro",
+          title: 'Bringing AI to Production - Intro',
+          description: "",
+          section: "Talks",handler: () => {
+              window.location.href = "/talks/imvc-25-ai-to-production/";
+            },},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
